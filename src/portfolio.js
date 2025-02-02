990e5e725cb3f854004f488dca254db8b6bcad96
RAMAKRISHNA REDDY VENNAM <96231389+RamaKrishnareddy17@users.noreply.github.com>
2025-02-02 02:00:01 -0500

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -4,7 +4,6 @@
 
 import emoji from "react-easy-emoji";
 import splashAnimation from "./assets/lottie/splashAnimation"; // Rename to your file name for custom animation
-import { TRUE } from "sass";
 
 // Splash Screen
 
